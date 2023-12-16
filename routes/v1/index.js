04c4fd8369cb41f258b6332af39715b121a9c368
KyleD8n <kyled8n@gmail.com>
2023-12-16 11:54:27 -0700

    updated routes index.js

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const login = require("./login");
+const recipe = require("./recipe");
 
 const router = express.Router();
+
+router.use("/login", login);
+router.use("/recipe", recipe);
 
 module.exports = router;
