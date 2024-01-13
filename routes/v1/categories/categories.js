9cfe08d93f6598fe0f51f2ed81063504deaac2e7
KyleD8n <kyled8n@gmail.com>
2024-01-13 12:27:40 -0700

    Category endpoint all set up and working with postman

---
+++ routes/v1/categories/categories.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const db = req.app.get("db");
+    const categories = await db.get_categories();
+
+    res.json(categories);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
